8e92a0db4a985aefa3d94afcf0188954bebd1a3a
arzikycreator-cell <arzikycreator@gmail.com>
2026-07-11 01:00:41 +0800

    Create app.js
    Mambuat app

---
+++ app.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
+}
+
+setTimeout(function(){
+
+window.location.replace(
+"https://script.google.com/macros/s/AKfycbzwspf0DNL02kFAsPv1H4AJjf8yV29FRR7crcYuW_dgYI2pRGKhKHColOn0kiBmFKnj9A/exec");
+
+},2500);
