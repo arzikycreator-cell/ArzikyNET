1695a56ee95e4e55bfec42f9b069db2e79644ada
arzikycreator-cell <arzikycreator@gmail.com>
2026-07-21 14:46:20 +0800

    Update app.js
    revisi tombol

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ if ('serviceWorker' in navigator) {
 setTimeout(function(){
 
 window.location.replace(
-"https://script.google.com/macros/s/AKfycbyNJAlCodlyff7prFIaNAngsVZAgbcqaoh220YZ6vE3lhBIyVWamiOJ74J6R5mxq23Xcg/exec");
+"https://script.google.com/macros/s/AKfycbz6aAgSZGEV-nBXugT5RupUCV0pAoysP29kbmKoMz6as4LZ46A0f2Vm_q3KVZu-Tg9OIA/exec");
 
 },2500);
