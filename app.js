38a95cadeaf3b8903d95ce66bd021a3806244dbd
arzikycreator-cell <arzikycreator@gmail.com>
2026-07-23 18:07:40 +0800

    Update app.js
    Rev03

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ if ('serviceWorker' in navigator) {
 setTimeout(function(){
 
 window.location.replace(
-"https://script.google.com/macros/s/AKfycbwe01EWVGFn0XzU_oL9qWLnr-57mx-aoJSzCfgWI_NCp9Vx5xqJs8oM0YV2G7erEdcRpA/exec");
+"https://script.google.com/macros/s/AKfycby2_SlzQKOAqNNnFTsAHpBdegbHHDVpzVNqr58b5swKQ2QxxlyHejcuZ8D87qLjW7Lcmw/exec");
 
 },2500);
